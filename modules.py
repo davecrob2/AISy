ba7891ee2372b9bb99eac6b4303311500663d74d
davecrob2 <davecrob2@gmail.com>
2017-09-23 16:11:01 -0500

    added documentation

--- modules.py
+++ modules.py
@@ -2,12 +2,15 @@ from tkinter import *
 from SystemClasses import invoice, purchaseOrder,customer
 import sqlite3
 
+#Function to connect to a specified database
 def db_connect(file):
     conn=sqlite3.connect(file)
     return(conn)
 
+#Window to enter Invoice details
 class apScreen():
     def __init__(self,master):
+        #Variable classes for entries
         self.amtvar=DoubleVar()
         self.numbervar=StringVar()
         self.datevar=StringVar()
@@ -16,19 +19,20 @@ class apScreen():
         self.custnumvar=StringVar()
        
         
-        
+        #Geometry, title, and location of window
         self.master=master
         self.master.geometry('500x300')
         self.master.title('Enter Invoice Details')
         
+        #Labels for invoice detail fields
         self.numberlabel=Label(self.master,text='Invoice Number').grid(row=0,column=1)
         self.amtlabel=Label(self.master,text='Invoice Amount').grid(row=1,column=1)
-        self.datelabel=Label(self.master,text='Invoice Date (MM/DD/YYY)').grid(row=2,column=1)
-        self.duedatelabel=Label(self.master,text='Due Date (MM/DD/YYY)').grid(row=3,column=1)
+        self.datelabel=Label(self.master,text='Invoice Date (MM/DD/YYYY)').grid(row=2,column=1)
+        self.duedatelabel=Label(self.master,text='Due Date (MM/DD/YYYY)').grid(row=3,column=1)
         self.custnamelabel=Label(self.master,text='Customer Name').grid(row=4,column=1)
         self.custnumlabel=Label(self.master,text='Customer Number').grid(row=5,column=1)
         
-        
+        #Entry fields for Invoice Details
         self.number=Entry(self.master,textvariable=self.numbervar).grid(row=0,column=2)
         self.amt=Entry(self.master,textvariable=self.amtvar).grid(row=1,column=2)
         self.date=Entry(self.master,textvariable=self.datevar).grid(row=2,column=2)
@@ -36,7 +40,7 @@ class apScreen():
         self.custname=Entry(self.master,textvariable=self.custnamevar).grid(row=4,column=2)
         self.custnum=Entry(self.master,textvariable=self.custnumvar).grid(row=5,column=2)
         
-        
+        #Button to save invoice, bound to the save invoice function
         self.button1=Button(self.master,text="Save Invoice",command=self.saveInvoice).grid(row=7,columnspan=3)
     
         
@@ -45,8 +49,10 @@ class apScreen():
         invoicedetails=invoice(self.numbervar.get(),self.amtvar.get(),self.datevar.get(),self.custnamevar.get(),self.custnumvar.get(),self.duedatevar.get())
         print(invoicedetails.custname)
 
+#Window to enter Purchase Order details
 class arScreen():
     def __init__(self,master):
+        #Variable classes for entries
         self.itemvar=StringVar()
         self.itemnumvar=StringVar()
         self.unitvar=StringVar()
@@ -56,10 +62,12 @@ class arScreen():
         self.custnumbervar=IntVar()
         self.povar=IntVar()
         
+        #Geometry, title, and window location
         self.master=master
         self.master.geometry('500x300')
         self.master.title('Enter Purchase Order Details')
 
+        #Labels for PO details fields
         self.polabel=Label(self.master,text="PO Number").grid(row=0,column=1)
         self.itemlabel=Label(self.master,text='Item Description').grid(row=1,column=1)
         self.itemnumlabel=Label(self.master,text='Item Number').grid(row=2,column=1)
@@ -69,6 +77,7 @@ class arScreen():
         self.custnamelabel=Label(self.master,text='Customer Name').grid(row=6,column=1)
         self.custnumlabel=Label(self.master,text='Customer Number').grid(row=7,column=1)
                 
+        #Entry fields for PO details
         self.po=Entry(self.master,textvariable=self.povar).grid(row=0,column=2)
         self.item=Entry(self.master,textvariable=self.itemvar).grid(row=1,column=2)
         self.itemnum=Entry(self.master,textvariable=self.itemnumvar).grid(row=2,column=2)
@@ -78,23 +87,28 @@ class arScreen():
         self.custname=Entry(self.master,textvariable=self.custnamevar).grid(row=6,column=2)
         self.custnum=Entry(self.master,textvariable=self.custnumbervar).grid(row=7,column=2)
 
-        self.button1=Button(self.master,text="Create PO",command=self.insert_row).grid(row=8,columnspan=3)
+        #Button to create PO
+        self.button1=Button(self.master,text="Create PO",command=self.create_PO).grid(row=8,columnspan=3)
     
         
-    def createPO(self):
-        current=purchaseOrder(self.itemvar.get(),self.itemnumvar.get(),self.quantityvar.get(),self.unitvar.get(),self.unitcostvar.get(),self.povar.get())
-        #current=customer(self.custnamevar.get(),self.custnumbervar.get(),0,0)
-
-        print(current.item)
-    def insert_row(self):
+    #Function to create the PO in the AISyDB database in table PurchaseOrders
+    def create_PO(self):
+        #Connecting to AISyDB..
         conn=db_connect("AISyDB.db")
+        
+        #Cursor creation
         c=conn.cursor()
+        
+        #Capturing entries from window into a list to post to db
         po=[self.povar.get(),self.itemvar.get(),self.itemnumvar.get(),self.quantityvar.get(),self.unitvar.get(),self.unitcostvar.get()]
         
+        #SQL commands
         c.execute("INSERT INTO PurchaseOrders VALUES (?,?,?,?,?,?)",po)
 
+        #Commit changes to db
         conn.commit()
 
+        #Close db
         conn.close()
 
 class glScreen():
